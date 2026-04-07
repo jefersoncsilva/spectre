57fd7653b08af6fc5b66f693876f82655ae28ba5
jefersom <jefersoncsilva14@gmail.com>
2026-04-07 17:58:54 -0300

    fix: corrige configuração SSL do mysqlclient para Aiven

--- spectre_project/settings.py
+++ spectre_project/settings.py
@@ -63,7 +63,7 @@ DATABASES = {
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
             'charset': 'utf8mb4',
-            'ssl_ca': config('DB_SSL_CA', default=''),
+            'ssl': {'ca': config('DB_SSL_CA', default='')},
         },
     }
 }
