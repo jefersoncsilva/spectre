4d2cf0f103940741b9ce0e35d835d2eeb9892f30
jefersom <jefersoncsilva14@gmail.com>
2026-04-13 18:20:10 -0300

    temp: redefine senha jeferson no deploy

--- build.sh
+++ build.sh
@@ -4,3 +4,4 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py shell -c "from django.contrib.auth.hashers import make_password; from core.models import Usuario; u=Usuario.objects.get(login='jeferson'); u.senha_hash=make_password('123456'); u.save(); print('Senha redefinida OK')"
